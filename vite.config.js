58618c59e702d8053ea0a3cc425c1d10b245eea7
Alvaro <velazquez.matus.alvaro@gmail.com>
2026-04-07 10:55:18 -0600

    se cambio a url la base de vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FinaMente/',
+  base: 'https://omarcillo44.github.io/FinaMente/',
 })
